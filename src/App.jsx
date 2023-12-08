b71a6796eb043b92ad8522bb8121b17313c04bbd
Everton-Fernandes <e.everton.elf@gmail.com>
2023-12-08 12:43:24 -0300

    Deploy fail

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 } from "./components";
 const App = () => {
   return (
-    <BrowserRouter path="/portifolio">
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to="/portifolio"
+          to="/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
